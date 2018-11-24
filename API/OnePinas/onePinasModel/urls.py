ae1b1d0640f0369055d8f6766ce2ae33c8540c49
granaderos <marejeanperpinosa@gmail.com>
2018-11-25 01:34:09 +0800

    API; Some back-end functions done.

---
+++ API/OnePinas/onePinasModel/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("api/login", views.login),
+    path("api/get_employee_tasks", views.get_employee_tasks),
+    path("api/change_task_status", views.change_task_status),
+    path("api/assign_task_to_employee", views.assign_task_to_employee),
+    
+
+]
